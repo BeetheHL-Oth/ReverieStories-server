49312d2c9d744bc28af19e450cb7936fee83c02b
Brandon Lawanto <banansbby@gmail.com>
2026-02-27 00:00:23 +0700

    added elevenlabs tts

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,58 +1,62 @@
-function errorHandler (error, req, res, next) {
+function errorHandler(error, req, res, next) {
   let code;
   let message;
-  switch(error.name) {
+  switch (error.name) {
     case 'SequelizeDatabaseError':
-      code = 400
-      message = 'Invalid input'
+      code = 400;
+      message = 'Invalid input';
       break;
     case 'SequelizeUniqueConstraintError':
-      code = 400
-      message = error.errors[0].message
+      code = 400;
+      message = error.errors[0].message;
       break;
     case 'SequelizeValidationError':
-      code = 400
-      message = error.errors[0].message
+      code = 400;
+      message = error.errors[0].message;
       break;
     case 'invalidPrompt':
-      code = 400
-      message = error.message
+      code = 400;
+      message = error.message;
       break;
     case 'LoginError':
-      code = 401
-      message = error.message
+      code = 401;
+      message = error.message;
       break;
     case 'notFound':
-      code = 404
-      message = error.message
+      code = 404;
+      message = error.message;
       break;
     case 'unauthorized':
-      code = 401
-      message = error.message
+      code = 401;
+      message = error.message;
       break;
     case 'forbidden':
-      code = 403
-      message = error.message
+      code = 403;
+      message = error.message;
       break;
     case 'invalidPrompt':
-      code = 400
-      message = error.message
+      code = 400;
+      message = error.message;
       break;
     case 'alreadyAdded':
-      code = 400
-      message = error.message
+      code = 400;
+      message = error.message;
       break;
     case 'googleLoginError':
-      code = 400
-      message = error.message
+      code = 400;
+      message = error.message;
+      break;
+    case 'elevenLabsError':
+      code = 502;
+      message = error.message;
       break;
     default:
-      code = 500
-      message = 'Internal Server Error'
+      code = 500;
+      message = 'Internal Server Error';
   }
   res.status(code).json({
-    message
-  })
+    message,
+  });
 }
 
-module.exports = errorHandler
+module.exports = errorHandler;
